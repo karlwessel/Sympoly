97ef5c7879d27e13bd2b6813e8ef418b2b7ffca1
Karl Royen <wessel.karl@googlemail.com>
2025-03-21 07:07:44 +0100

    add some newlines to inspection output

--- src/inspection.jl
+++ src/inspection.jl
@@ -1,45 +1,51 @@
 function inspect(p::Polyform, io = stdout)
-	if isone(p.denom)
-		if length(p.p) == 0
-			print(io, "0")
-			return
-		end
-		facs = factor(data(p.p))
-		!isone(unit(facs)) && print(io, unit(facs))
-		for (f, e) in facs
-			length(facs) > 1 && length(terms(f)) > 1 &&print(io, "(")
-			termsperline = 7
-			charactersperline = 30
-			for (i, t) in enumerate(terms(f))
-				termsperline -= 1
-				r = repr(t)
-				charactersperline -= length(r)
-				if i > 1
-					if termsperline < 0 || charactersperline < 0
-						print(io, "\n ")
-						termsperline = 7
-						charactersperline = max(0, charactersperline + 30)
-					end
-					print(io, " + ")
-				end
-				print(io, r)
-			end
-			length(facs) > 1 && length(terms(f)) > 1 && print(io, ")")
-			!isone(e) && print(io, "^$e")
-		end
-	else
-		q, r = divrem(p.p, p.denom)
-		gc = gcd(r, p.denom)
-		if !iszero(q)
-	        inspect(Polyform(q, one(p.p), p.fns), io)
-		end
-		if !iszero(r)
-			!iszero(q) && print(io,"\n + ")
-			print(io, "(")
-			inspect(Polyform(r / gc, one(p.p), p.fns), io)
-			print(io,"\n) / ( \n ")
-	        inspect(Polyform(p.denom / gc, one(p.p), p.fns), io)
-			print(io, "\n)")
-		end
-	end
+    if isone(p.denom)
+        if length(p.p) == 0
+            print(io, "0")
+            return
+        end
+        facs = factor(data(p.p))
+        !isone(unit(facs)) && print(io, unit(facs))
+        for (f, e) in facs
+            length(facs) > 1 && length(terms(f)) > 1 &&print(io, "[")
+            termsperline = 7
+            charactersperline = 30
+            for (i, t) in enumerate(terms(f))
+                termsperline -= 1
+                r = repr(t)
+                charactersperline -= length(r)
+                if i > 1
+                    if termsperline < 0 || charactersperline < 0
+                        print(io, "\n ")
+                        termsperline = 7
+                        charactersperline = max(0, charactersperline + 30)
+                    end
+                    if first(r) == '-'
+                        r = r[2:end]
+                        print(io, " - ")
+                    else
+                        print(io, " + ")
+                    end
+                end
+                print(io, r)
+            end
+            length(facs) > 1 && length(terms(f)) > 1 && print(io, "]")
+            !isone(e) && print(io, "^$e")
+        end
+        print(io, "\n")
+    else
+        q, r = divrem(p.p, p.denom)
+        if !iszero(q)
+            inspect(Polyform(q, one(p.p), p.fns), io)
+        end
+        if !iszero(r)
+            gc = gcd(r, p.denom)* gcd(content(r), content(p.denom))
+            !iszero(q) && print(io," + ")
+            print(io, "(")
+            inspect(Polyform(r / gc, one(p.p), p.fns), io)
+            print(io,") / ( \n ")
+            inspect(Polyform(p.denom / gc, one(p.p), p.fns), io)
+            print(io, "\n)")
+        end
+    end
 end
